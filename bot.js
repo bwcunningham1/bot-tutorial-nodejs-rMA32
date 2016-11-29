611f185c005eb92b1d75e0fc528f1b13d1d7cc85
bwcunningham1 <bwcunningham1@mail.lipscomb.edu>
2016-11-28 22:37:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,4 @@
 {
   "bot_id"  : "36c3905e18ad0b85ae35c5bd39",
-  "text"    : "Hello world",
-  "attachments" : [
-    {
-      "type"  : "image",
-      "url"   : "http://www.steveirwinday.org/assets/img/home/home_steve_irwin.jpg"
-    }
-  ]
+  "text"    : "Hello world"
 }
